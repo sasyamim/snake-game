f2e7e0839ace442e72468c64eabd4830d9f7c857
sas <sasyamim.business@gmail.com>
2024-02-29 14:43:23 +0800

    first commit

---
+++ Snake Game.cpp
@@ -0,0 +1,11 @@
+// Snake Game.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!" << endl;
+}
